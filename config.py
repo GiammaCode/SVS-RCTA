418d1026499a71514ff9190497f108499e5e6511
Gianmaria Casamenti - gianmaria.casamenti@studio.unibo.it <gianmaria.casamenti@studio.unibo.it>
2025-10-28 10:40:54 +0100

    fix: update target camion transform

--- config.py
+++ config.py
@@ -21,29 +21,29 @@ BLOCKING_VEHICLE_MODELS = [
 ]
 
 EGO_SPAWN_TRANSFORM = carla.Transform(
-    carla.Location(x = 184.5, y=-5.5, z = 0.5),
-    carla.Rotation(yaw=180)  #ready to reverse direction (retromarcia)
+    carla.Location(x=-30.0, y=-30, z=0.5),
+    carla.Rotation(yaw=0)  #ready to reverse direction (retromarcia)
 )
 
 BLOCKING_VEHICLE_TRANSFORMS = [
     carla.Transform(
-        carla.Location(x=-184.5, y=-3.0, z=0.5),
-        carla.Rotation(yaw=180)
+        carla.Location(x=-30.0, y=-27.0, z=0.5),
+        carla.Rotation(yaw=0)
     ),
     carla.Transform(
-        carla.Location(x=-184.5, y=-8.0, z=0.5),
-        carla.Rotation(yaw=180)
+        carla.Location(x=-30.0, y=-32.8, z=0.5),
+        carla.Rotation(yaw=0)
     ),
      carla.Transform(
-        carla.Location(x=-184.5, y=-10.5, z=0.5),
-        carla.Rotation(yaw=180)
+        carla.Location(x=-30.0, y=-35.5, z=0.5),
+        carla.Rotation(yaw=0)
     )
 ]
 
-TARGET_VEHICLE_MODEL = 'vehicle.coca_cola.truck'
-#spawn sulla strada principale (x ~ -175), ma a y=20 (fuori dalla vista iniziale)
+TARGET_VEHICLE_MODEL = 'vehicle.mercedes.sprinter'
 TARGET_SPAWN_TRANSFORM = carla.Transform(
-    carla.Location(x=-175.0, y=20.0, z=0.5),
+    carla.Location(x=-35.0, y=-40.0, z=0.5),  # Posizione di spawn (aggiornata per il nuovo parcheggio)
+    carla.Rotation(yaw=90)                  # Rotazione (verso sud, asse Y negativo)
 )
 # Si muove a 8 m/s (circa 29 km/h) lungo l'asse Y negativo.
 TARGET_VELOCITY = carla.Vector3D(x=0, y=-8.0, z=0)
