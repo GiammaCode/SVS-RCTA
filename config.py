8c4effa61f4571b27ea17f11da774466b5e3dfcf
Gianmaria Casamenti - gianmaria.casamenti@studio.unibo.it <gianmaria.casamenti@studio.unibo.it>
2025-10-28 10:48:11 +0100

    chore: refact config

--- config.py
+++ config.py
@@ -9,22 +9,23 @@ HOST = 'localhost'
 PORT = 2000
 TIMEOUT = 10
 
-#Scenarios
+#Scenarios setting
 MAP_NAME = 'Town05'
 
+#EGO vehicle
 EGO_VEHICLE_MODEL = 'vehicle.audi.tt'
+EGO_SPAWN_TRANSFORM = carla.Transform(
+    carla.Location(x=-30.0, y=-30, z=0.5),
+    carla.Rotation(yaw=0)  #ready to reverse direction (retromarcia)
+)
+
+#blocking vehicles
 BLOCKING_VEHICLE_MODELS = [
     'vehicle.toyota.prius',
     'vehicle.nissan.patrol',
     'vehicle.ford.mustang',
     'vehicle.chevrolet.impala'
 ]
-
-EGO_SPAWN_TRANSFORM = carla.Transform(
-    carla.Location(x=-30.0, y=-30, z=0.5),
-    carla.Rotation(yaw=0)  #ready to reverse direction (retromarcia)
-)
-
 BLOCKING_VEHICLE_TRANSFORMS = [
     carla.Transform(
         carla.Location(x=-30.0, y=-27.0, z=0.5),
@@ -40,13 +41,15 @@ BLOCKING_VEHICLE_TRANSFORMS = [
     )
 ]
 
+
+#target vehicle
 TARGET_VEHICLE_MODEL = 'vehicle.mercedes.sprinter'
 TARGET_SPAWN_TRANSFORM = carla.Transform(
-    carla.Location(x=-35.0, y=-40.0, z=0.5),  # Posizione di spawn (aggiornata per il nuovo parcheggio)
-    carla.Rotation(yaw=90)                  # Rotazione (verso sud, asse Y negativo)
+    carla.Location(x=-35.0, y=-40.0, z=0.5),
+    carla.Rotation(yaw=90)
 )
-# Si muove a 8 m/s (circa 29 km/h) lungo l'asse Y negativo.
-TARGET_VELOCITY = carla.Vector3D(x=0, y=-8.0, z=0)
+#2 m/s (circa 7 km/h)
+TARGET_VELOCITY = carla.Vector3D(x=0, y=2.0, z=0)
 
 
 
